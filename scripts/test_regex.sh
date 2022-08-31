f1bf57c3da0ff8e525a1811008f8c7cbd2f89333
vaaandark <vaaandark@gmail.com>
2022-08-31 14:44:42 +0800

    test: 加入了更多常用正则的测试

--- scripts/test_regex.sh
+++ scripts/test_regex.sh
@@ -29,6 +29,23 @@ run 'a+|aab*' 'aab' 'aab'
 run 'ab*c' 'ac' 'ac'
 run 'x(ab)*y' '' ''
 
+# email
+run '[a-zA-Z0-9_-]+@[a-zA-Z0-9_-]+(\.[a-zA-Z0-9_-]+)+' \
+  'vaaandark@outlook.com' 'vaaandark@outlook.com'
+
+# mobile phone number
+run '1(3|4|5|6|7|8|9)\d{9}' '13800000000' '13800000000'
+
+# domain name
+run '((http:\/\/)|(https:\/\/))?([a-zA-Z0-9]([a-zA-Z0-9\-]{0,61}[a-zA-Z0-9])?\.)+[a-zA-Z]{2,6}(\/)' \
+  'https://google.com/' 'https://google.com/'
+run '((http:\/\/)|(https:\/\/))?([a-zA-Z0-9]([a-zA-Z0-9\-]{0,61}[a-zA-Z0-9])?\.)+[a-zA-Z]{2,6}(\/)' \
+  'https://google.com' ''
+
+run '[1-9]\d*\.\d*|0\.\d*[1-9]\d*' '3.14' '3.14'
+run '[1-9]\d*\.\d*|0\.\d*[1-9]\d*' '-3.14' ''
+run '[1-9]\d*\.\d*|0\.\d*[1-9]\d*' '3' ''
+
 echo "$success SUCCESS"
 echo "$failure FAILURE"
 echo "$(( 100 * success / (success + failure) ))% passed"
