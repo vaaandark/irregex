3fcfb504d067083c02e09a99ac137afe56013aee
vaaandark <vaaandark@gmail.com>
2022-09-01 14:10:52 +0800

    style: 修改测试用例文件名，避免进入GitHub的代码统计中

--- bench/bench.sh
+++ bench/bench.sh
@@ -4,7 +4,8 @@ cd "$(dirname "$0")" || exit 1
 gcc lexer.c -O2 -o ../build/posixre-lexer
 cd ..
 
-hyperfine --warmup 3 './build/lexer ./bench/huge.c' './build/posixre-lexer ./bench/huge.c'
+hyperfine --warmup 3 './build/lexer ./bench/huge_c_source' \
+  './build/posixre-lexer ./bench/huge_c_source'
 
 rm ./build/posixre-lexer
 
